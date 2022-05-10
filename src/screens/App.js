4a7aba98e8f1cae7fdc3ca48d4d6b2029b87fc79
Yu357 <forestgreen14689@gmail.com>
2022-05-10 18:44:54 +0900

    feat: Firestoreのドキュメントを取得できた

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,9 +1,19 @@
-import firebase, { db } from '../utilities/Firebase';
+import { collection, getDocs } from "firebase/firestore";
+import db from '../utilities/Firebase'
 
 function App() {
+  
+  async function read() {
+    const querySnapshot = await getDocs(collection(db, "memos"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  }
+
   return (
     <div className="App">
       <h1>Hello world.</h1>
+      <button onClick={read}>Read</button>
     </div>
   );
 }
