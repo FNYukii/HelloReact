230410beb2f5c85d84d863f40c67a6638f44addb
Yu357 <forestgreen14689@gmail.com>
2022-05-10 20:52:41 +0900

    feat: ReadViewをクラスコンポーネントに変更

--- src/screens/ReadView.js
+++ src/screens/ReadView.js
@@ -1,21 +1,27 @@
+import React from 'react'
 import { collection, getDocs } from "firebase/firestore";
 import db from '../utilities/Firebase'
 
-function ReadView() {
-  
-  async function read() {
+class ReadView extends React.Component {
+
+  read = async () => {
     const querySnapshot = await getDocs(collection(db, "memos"));
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()}`);
     });
   }
+  
+  render() {
+    const read = this.read()
 
-  return (
-    <div>
-      <h1>Read</h1>
-      <button onClick={read}>Read</button>
-    </div>
-  );
+    return (
+      <div>
+        <h1>Read</h1>
+        <button onClick={read}>Read</button>
+      </div>
+    );
+  }
+  
 }
 
 export default ReadView;
