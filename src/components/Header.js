291828737aa7842b4b791aab496dd5bf837f5532
Yu357 <forestgreen14689@gmail.com>
2022-05-12 11:38:12 +0900

    feat: 警告に対処

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,11 @@ function Header() {
     <header>
       <a href='/' className='logo'>React Fire</a>
       <ul>
-        <li><NavLink to="/" activeClassName='active' exact={true}>Read</NavLink></li>
-        <li><NavLink to="/read-realtime" activeClassName='active'>Read Realtime</NavLink></li>
-        <li><NavLink to="/create" activeClassName='active'>Create</NavLink></li>
-        <li><NavLink to="/update" activeClassName='active'>Update</NavLink></li>
-        <li><NavLink to="/delete" activeClassName='active'>Delete</NavLink></li>
+        <li><NavLink to="/" activeclassname='active' exact='true'>Read</NavLink></li>
+        <li><NavLink to="/read-realtime" activeclassname='active'>Read Realtime</NavLink></li>
+        <li><NavLink to="/create" activeclassname='active'>Create</NavLink></li>
+        <li><NavLink to="/update" activeclassname='active'>Update</NavLink></li>
+        <li><NavLink to="/delete" activeclassname='active'>Delete</NavLink></li>
       </ul>
     </header>
   )
