2770e10448fe224171566b4085608ee0c6fff956
Yu357 <forestgreen14689@gmail.com>
2022-05-12 11:35:46 +0900

    feat: ドキュメント追加

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { collection, addDoc } from 'firebase/firestore'; 
+import db from '../utilities/Firebase';
 
 class Create extends React.Component {
 
-  create() {
-    // TODO: Create new document on Cloud Firestore
+  async create() {
+    const docRef = await addDoc(collection(db, 'users'), {
+      displayName: 'Ayumu',
+      userName: 'Ayumu_Satonaka'
+    });
+    console.log('Document written with ID: ', docRef.id);
   }
 
   render() {
     return (
       <main>
         <h2>Create</h2>
+        <button onClick={this.create}>Add</button>
       </main>
     );
   }
