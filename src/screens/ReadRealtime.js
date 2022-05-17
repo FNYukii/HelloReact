a02d324f4f470ff2616b4308d5719d90d4456da2
Yu357 <forestgreen14689@gmail.com>
2022-05-17 15:00:01 +0900

    feat: docsを取得できた

--- src/screens/ReadRealtime.js
+++ src/screens/ReadRealtime.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import db from '../utilities/Firebase';
 
 class ReadRealtime extends React.Component {
+
+  state = {
+    documents: []
+  }
+
+  listen() {
+    const q = query(collection(db, "users"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push(doc);
+        console.log(`id: ${doc.id}, displayName: ${doc.data().displayName}, userName: ${doc.data().userName}`);
+      });
+
+      this.setState({
+        documents: docs
+      })
+    });
+  }
+
+  componentDidMount() {
+    this.listen();
+  }
 
   render() {
     return (
