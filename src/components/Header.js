88669d678a80103c383225f54a90bca48b864c0f
Yu357 <forestgreen14689@gmail.com>
2022-05-19 10:33:09 +0900

    feat: global-navからUpdateとDeleteの項目を削除

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,8 +16,6 @@ function Header() {
           <li><NavLink to="/" activeclassname='active' exact='true'>Read</NavLink></li>
           <li><NavLink to="/read-realtime" activeclassname='active'>Read Realtime</NavLink></li>
           <li><NavLink to="/create" activeclassname='active'>Create</NavLink></li>
-          <li><NavLink to="/update" activeclassname='active'>Update</NavLink></li>
-          <li><NavLink to="/delete" activeclassname='active'>Delete</NavLink></li>
           <li><a href='https://console.firebase.google.com/u/0/project/reactfire-b8b73/firestore' target="blank">Console <BsBoxArrowUpRight/></a></li>
         </ul>
 
