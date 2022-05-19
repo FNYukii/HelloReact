e7423ff2d35f55651e2322b7eb6054b462a9a4e8
Yu357 <forestgreen14689@gmail.com>
2022-05-19 11:09:18 +0900

    feat: UI部品を配置

--- src/screens/Update.js
+++ src/screens/Update.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Update(props) {
 
   let { id } = useParams();
+  const [displayName, setDisplayName] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const onInputDisplayName = (e) => {
+    setDisplayName(e.target.value);
+  }
+
+  const onInputUserName = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const update = () => {
+    // TODO: Update
+  }
 
   return (
     <main>
       <div className='large-container'>
         <h2>Update</h2>
 
-        <p>DocumentID: {id}</p>
+        <p>Document ID : {id}</p>
+
+        <form>
+          <div className='form-item'>
+            <span>Display name</span>
+            <input value={displayName} onChange={onInputDisplayName}/>
+          </div>
+
+          <div className='form-item'>
+            <span>User name</span>
+            <input value={userName} onChange={onInputUserName}/>
+          </div>
+
+        </form>
+
+        <button onClick={update}>Update</button>
 
       </div>
     </main>
