945a4c1a16422ab04c9e4c972ac1c7b52fdb7637
Yu357 <forestgreen14689@gmail.com>
2022-05-20 13:27:54 +0900

    feat: リンク修正

--- src/screens/ReadRealtime.js
+++ src/screens/ReadRealtime.js
@@ -38,7 +38,7 @@ function ReadRealtime() {
                 <p className='user-name'>@{document.data().userName}</p>
 
                 <a href={'/update/' + document.id} className='update-link'>Update</a>
-                <a href='/' className='delete-link'>Delete</a>
+                <a href={'/delete/' + document.id} className='delete-link'>Delete</a>
               </div>
             ))
           }
