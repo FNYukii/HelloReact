136c05df834c839d1fe0e753d28981779a8387c6
Yu357 <forestgreen14689@gmail.com>
2022-05-20 13:14:08 +0900

    feat: Deleteページへの遷移を実装

--- src/screens/Delete.js
+++ src/screens/Delete.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function Delete() {
+
+  // Properties
+  let { id } = useParams();
   
+  // View
   return (
     <main>
       <div className='large-container'>
         <h2>Delete</h2>
+
+        <p>id: {id}</p>
+        
 
       </div>
     </main>
