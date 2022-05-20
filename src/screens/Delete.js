ab3f2720917c23db509434ac8c7fc0003227ab02
Yu357 <forestgreen14689@gmail.com>
2022-05-20 13:22:31 +0900

    feat: DeleteページにdisplayNameとuserNameを表示

--- src/screens/Delete.js
+++ src/screens/Delete.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, getDoc } from "firebase/firestore";
 import db from '../utilities/Firebase';
 
 function Delete() {
@@ -8,12 +8,32 @@ function Delete() {
   // Properties
   let { id } = useParams();
   const navigate = useNavigate();
+  const [displayName, setDisplayName] = useState('');
+  const [userName, setUserName] = useState('');
+
+  // Read
+  async function read() {
+    const docRef = doc(db, "users", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setDisplayName(docSnap.data().displayName);
+      setUserName(docSnap.data().userName);
+    } else {
+      console.log("No such document!");
+    }
+  }
 
   // Delete
   async function deleteUser() {
     await deleteDoc(doc(db, "users", id));
     navigate(-1);
   }
+
+  useEffect(() => {
+    read();
+    //eslint-disable-next-line
+  }, []);
   
   // View
   return (
@@ -21,8 +41,10 @@ function Delete() {
       <div className='large-container'>
         <h2>Delete</h2>
 
-        <p>id: {id}</p>
-        
+        <p>Document ID : {id}</p>
+        <p>Display name : {displayName}</p>
+        <p>User name : {userName}</p>
+
         <button onClick={deleteUser}>Delete</button>
 
       </div>
