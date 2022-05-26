e19b7ff26510634f430461bd4d26de7131d29a6d
Yu357 <forestgreen14689@gmail.com>
2022-05-26 20:39:29 +0900

    feat: アプリ名をHello Reactに変更

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import '../styles/header.css';
 import { NavLink } from 'react-router-dom';
-import { BsBoxArrowUpRight } from 'react-icons/bs';
 import { FaBars } from 'react-icons/fa';
 
 function Header() {
@@ -8,7 +7,7 @@ function Header() {
   return (
     <header>
       <div className='large-container'>
-        <a href='/' className='logo'>React Fire</a>
+        <a href='/' className='logo'>Hello React</a>
 
         <FaBars className='bars'/>
 
@@ -16,7 +15,6 @@ function Header() {
           <li><NavLink to="/" activeclassname='active' exact='true'>Read</NavLink></li>
           <li><NavLink to="/read-realtime" activeclassname='active'>Read Realtime</NavLink></li>
           <li><NavLink to="/create" activeclassname='active'>Create</NavLink></li>
-          <li><a href='https://console.firebase.google.com/u/0/project/reactfire-b8b73/firestore' target="blank">Console <BsBoxArrowUpRight/></a></li>
         </ul>
 
       </div>
